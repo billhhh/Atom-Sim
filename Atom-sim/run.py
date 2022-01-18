5088cc06132053b3708c42b7c24a5c2dfbfb885b
Bill Wang <billhhh@users.noreply.github.com>
2022-01-18 15:15:54 +1030

    coords gen

--- Atom-sim/run.py
+++ Atom-sim/run.py
@@ -1,6 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_points_with_min_distance(n, shape, min_dist):
+    # compute grid shape based on number of points
+    width_ratio = shape[1] / shape[0]
+    num_y = np.int32(np.sqrt(n / width_ratio)) + 1
+    num_x = np.int32(n / num_y) + 1
+
+    # create regularly spaced neurons
+    x = np.linspace(0., shape[1]-1, num_x, dtype=np.float32)
+    y = np.linspace(0., shape[0]-1, num_y, dtype=np.float32)
+    coords = np.stack(np.meshgrid(x, y), -1).reshape(-1,2)
+
+    # compute spacing
+    init_dist = np.min((x[1]-x[0], y[1]-y[0]))
+
+    # perturb points
+    max_movement = (init_dist - min_dist)/2
+    noise = np.random.uniform(low=-max_movement,
+                                high=max_movement,
+                                size=(len(coords), 2))
+    coords += noise
+
+    return coords
+
 
 def main():
-    pass
+    w = 100
+    h = 100
+    diameter = 1.5
+    red_num = 50
+    black_num = 50
+    trials = 10
+
+    coords = generate_points_with_min_distance(n=red_num, shape=(w, h), min_dist=diameter)
+
+    # plot
+    plt.figure(figsize=(10, 10))
+    plt.scatter(coords[:, 0], coords[:, 1], s=3)
+    plt.show()
 
 
 if __name__ == '__main__':
